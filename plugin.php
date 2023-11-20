fb5cf4a875009fd2ccdc1413c8f6abdac8069f8e
Tomislav Zecevic <tz027@hdm-stuttgart.de>
2023-11-20 11:57:30 +0100

    revert changes

--- plugin.php
+++ plugin.php
@@ -26,19 +26,6 @@ function custom_room_selection_load_start_index()
     return $start_index;
 }
 
-// Function to get the assigned user for a specific week and year
-function custom_room_selection_get_assigned_user($week, $year)
-{
-    $assigned_users_file = plugin_dir_path(__FILE__) . "assigned-users.json";
-    $assigned_users = file_exists($assigned_users_file) ? json_decode(file_get_contents($assigned_users_file), true) : [];
-
-    if (isset($assigned_users[$year][$week])) {
-        return $assigned_users[$year][$week];
-    }
-
-    return null;
-}
-
 // Retrieve selected room and next rooms
 function custom_room_selection_get_rooms()
 {
@@ -47,7 +34,20 @@ function custom_room_selection_get_rooms()
 
     // Sort users by name and room number
     usort($users, function ($userA, $userB) {
-        // Sorting logic
+        $nameA = $userA->display_name;
+        $nameB = $userB->display_name;
+
+        // Extract room numbers
+        $roomNumberA = intval(preg_replace('/[^0-9]+/', '', $nameA));
+        $roomNumberB = intval(preg_replace('/[^0-9]+/', '', $nameB));
+
+        // Compare room numbers
+        if ($roomNumberA !== $roomNumberB) {
+            return $roomNumberA - $roomNumberB;
+        }
+
+        // If room numbers are the same, compare names
+        return strcasecmp($nameA, $nameB);
     });
 
     // Filter out ignored users
@@ -67,30 +67,8 @@ function custom_room_selection_get_rooms()
     // Rearrange the room entries in the array to have the starting room at the first position
     $available_rooms = array_merge(array_slice($available_rooms, $start_index), array_slice($available_rooms, 0, $start_index));
 
-    // Determine current week and year
+    // Determine the current week
     $current_week = date('W');
-    $current_year = date('Y');
-
-    // Get assigned user for the current week and year
-    $assigned_user = custom_room_selection_get_assigned_user($current_week, $current_year);
-
-    // If assigned user exists, use it for this week
-    if ($assigned_user !== null) {
-        $next_rooms = array();
-        $next_week = $current_week + 1;
-        while (count($next_rooms) < 5) {
-            $next_room = $available_rooms[$next_week % count($available_rooms)];
-            if (!in_array($next_room, $next_rooms)) {
-                $next_rooms[] = $next_room;
-            }
-            $next_week++;
-        }
-
-        return array(
-            'selected' => $assigned_user,
-            'next'     => $next_rooms,
-        );
-    }
 
     // Calculate the selected room for this week
     $selected_room = $available_rooms[$current_week % count($available_rooms)];
@@ -159,19 +137,4 @@ function custom_room_selection_get_date_range($week_number)
     $start_of_week = date('d.m.Y', strtotime($year . 'W' . $week_number . '1'));
     $end_of_week = date('d.m.y', strtotime($year . 'W' . $week_number . '7'));
     return $start_of_week . ' - ' . $end_of_week;
-}
-
-// Function to assign a user to a specific week and year
-function custom_room_selection_assign_user($username, $week, $year)
-{
-    $assigned_users_file = plugin_dir_path(__FILE__) . 'assigned-users.json';
-    $assigned_users = file_exists($assigned_users_file) ? json_decode(file_get_contents($assigned_users_file), true) : array();
-
-    if (!isset($assigned_users[$year])) {
-        $assigned_users[$year] = array();
-    }
-
-    $assigned_users[$year][$week] = $username;
-
-    file_put_contents($assigned_users_file, json_encode($assigned_users));
 }
